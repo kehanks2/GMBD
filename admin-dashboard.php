02f5656e60d0a2c2c493143ac064f8997ef9edab
Kayla Hanks <kwardle90@gmail.com>
2020-11-29 13:51:11 -0500

    update to admin-dashboard

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -32,18 +32,57 @@ if ($_SESSION['user_type'] != 'Admin') {
 
         <div class="container" style="padding-top:30px">
 
-            <!-- admin table -->
+            <!-- select usertype -->
+            <div class="row form-inline">		
+                <div class="form-group">
+                    <select class="form-control" id="select-usertype">
+                        <option selected disabled>Select a usertype</option>
+                        <option value="Artist" id="artist">Artist</option>
+                        <option value="Business" id="business">Business</option>
+                        <option value="Venue" id="venue">Venue</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <button type="button" id="go-to-usertype" class="btn btn-success">
+                        View Users
+                    </button>
+                </div>
+            </div>
             <div class="row">
-                <div class="col-sm-12">
-                    <table id="admin-table" class="table table-striped" style="width:100%;">
-                        <thead>
-                            <tr>
-                                <th>Profile Name</th>
-                                <th>Account Type</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                    </table>
+                <div class="col-sm-12">              
+                    <div hidden id="show-artist-table">
+                        <table id="artist-table" class="table table-striped" style="width:100%;">
+                            <thead>
+                                <tr>
+                                    <th>Artist Name</th>
+                                    <th>Status</th>
+                                    <th>Update Status</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>    
+                    <div hidden id="show-business-table">
+                        <table id="business-table" class="table table-striped" style="width:100%;">
+                            <thead>
+                                <tr>
+                                    <th>Business Name</th>
+                                    <th>Status</th>
+                                    <th>Update Status</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>  
+                    <div hidden id="show-venue-table">
+                        <table id="venue-table" class="table table-striped" style="width:100%;">
+                            <thead>
+                                <tr>
+                                    <th>Venue Name</th>
+                                    <th>Status</th>
+                                    <th>Update Status</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -61,17 +100,66 @@ if ($_SESSION['user_type'] != 'Admin') {
             fetch_data();
 
             function fetch_data() {
-                var dataTable = $('#admin-table').DataTable({
-                    "processing": true,
-                    "serverSide": true,
-                    "dom": '<"top"f>t<"bottom"ip>',
-                    "order": [],
-                    "ajax": {
-                        url: "include/fetch-admin.php",
-                        type: "POST"
-                    }
-                });
+                if ($('#show-artist-table').is('visible')) {
+                    var dataTable = $('#artist-table').DataTable({
+                        "processing": true,
+                        "serverSide": true,
+                        "dom": '<"top"f>t<"bottom"ip>',
+                        "order": [],
+                        "ajax": {
+                            url: "include/fetch-admin.php",
+                            type: "POST",
+                            data: { table: "Artist" }
+                        }
+                    });
+                } else if ($('#show-business-table').is('visible')) {
+                    var dataTable = $('#business-table').DataTable({
+                        "processing": true,
+                        "serverSide": true,
+                        "dom": '<"top"f>t<"bottom"ip>',
+                        "order": [],
+                        "ajax": {
+                            url: "include/fetch-admin.php",
+                            type: "POST",
+                            data: { table: "Business" }
+                        }
+                    });
+                } else if ($('#show-venue-table').is('visible')) {
+                    var dataTable = $('#venue-table').DataTable({
+                        "processing": true,
+                        "serverSide": true,
+                        "dom": '<"top"f>t<"bottom"ip>',
+                        "order": [],
+                        "ajax": {
+                            url: "include/fetch-admin.php",
+                            type: "POST",
+                            data: { table: "Venue" }
+                        }
+                    });
+                }                
             }; 
+
+            // determines which usertype table is shown to the admin
+            $('#go-to-usertype').click(function() {
+                var usertype = $('#select-usertype').val();
+                if (usertype == 'Artist') {
+                    $('#show-artist-table').removeAttr('hidden');
+                    $('#show-business-table').prop('hidden', true);
+                    $('#show-venue-table').prop('hidden', true);
+                } else if (usertype == 'Business') {
+                    $('#show-business-table').removeAttr('hidden');
+                    $('#show-artist-table').prop('hidden', true);
+                    $('#show-venue-table').prop('hidden', true);
+                } else if (usertype == 'Venue') {
+                    $('#show-venue-table').removeAttr('hidden');
+                    $('#show-artist-table').prop('hidden', true);
+                    $('#show-business-table').prop('hidden', true);
+                } else {
+                    return;
+                }
+			});
+
+        })
     </script>
 </body>
 </html>
--- include/fetch-admin.php
+++ include/fetch-admin.php
@@ -5,73 +5,211 @@ ini_set('display_errors', true);
 
 include('config.php');
 
-$columns = array('VenueName', 'Capacity', 'PhoneNumber', 'Email', 'City', 'State');
+// artist table
+if ($_POST['table'] == 'Artist') {
+    $columns = array('artistName', 'Status');
 
-$query = "SELECT * FROM Venue WHERE (Status = 1) ";
-
-//--------------- FILTER---------------//
-if(isset($_POST['filter_capacity']) && $_POST['filter_capacity'] != '')
-{
- $query .= '
- WHERE Capacity = "'.$_POST['filter_capacity'].'" 
- ';
-}
+    $query = "SELECT * FROM Artist ";
 
 
-if(isset($_POST["search"]["value"])) {
-    $query .= ' 
-        AND (VenueName LIKE "%'.$_POST["search"]["value"].'%"
-        OR City LIKE "%'.$_POST["search"]["value"].'%" 
-        OR ZipCode LIKE "%'.$_POST["search"]["value"].'%") 
+    if(isset($_POST["search"]["value"])) {
+        $query .= ' 
+            WHERE artistName LIKE "%'.$_POST["search"]["value"].'%"
+            OR Status LIKE "%'.$_POST["search"]["value"].'%" 
+            ';
+    }
+
+    if (isset($_POST["order"])) {
+        $query .= ' ORDER BY '.$columns[$_POST['order']['0']['column']].' '.$_POST['order']['0']['dir'].' 
         ';
+    } else {
+        $query .= ' ORDER BY Status ASC ';
+    }
+
+    $query1 = '';
+
+    if ($_POST["length"] != -1) {
+        $query1 .= 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+    }
+
+    $number_filter_row = mysqli_num_rows(mysqli_query($db, $query));
+
+    $result = mysqli_query($db, $query . $query1);
+
+    $data = array();
+
+    while ($row = mysqli_fetch_array($result)) {
+        $sub_array = array();
+        $status = '';
+        if ($row['Status'] == 0) {
+            $status = "Pending";
+        } else if ($row['Status'] == 1) {
+            $status = "Approved";
+        } else {
+            $status = "Denied";
+        }
+
+        $sub_array[] = '<div class="update" data-id="'.$row["artistId"].'" data-column="'.$row['artistName'].'"><a href="#" id="profile-name">'.$row['artistName'].'</a></div>';
+        $sub_array[] = '<div class="update" data-id="'.$row["artistId"].'" data-column="'.$row['Status'].'">'.$status.'</div>';
+        $sub_array[] = '<div class="btn-group" role="group" data-id="'.$row["artistId"].'">
+                        <button type="button" name="approve" id="approve" data-toggle="tooltip" title="Approve User" class="btn btn-success">Approve</button>
+                        <button type="button" name="pending" id="pending" data-toggle="tooltip" title="User Pending" class="btn btn-warning">Pending</button>
+                        <button type="button" name="deny" id="deny" data-toggle="tooltip" title="Deny User" class="btn btn-danger">Deny</button></div></td>';
+
+        $data[] = $sub_array;   
+    }
+
+    function get_all_data($db) {
+        $query = "SELECT * FROM Artist";
+        $result = mysqli_query($db, $query);
+        return mysqli_num_rows($result);
+    }
+
+    $output = array(
+        "draw"              =>  intval($_POST["draw"]),
+        "recordsTotal"      =>  get_all_data($db),
+        "recordsFiltered"   =>  $number_filter_row,
+        "data"              =>  $data
+    );
+
+    echo json_encode($output);
+
+    // business table
+} else if ($_POST['table'] == 'Business') {
+    $columns = array('BusinessName', 'Status');
+
+    $query = "SELECT * FROM Business ";
+
+
+    if(isset($_POST["search"]["value"])) {
+        $query .= ' 
+            WHERE BusinessName LIKE "%'.$_POST["search"]["value"].'%"
+            OR Status LIKE "%'.$_POST["search"]["value"].'%" 
+            ';
+    }
+
+    if (isset($_POST["order"])) {
+        $query .= ' ORDER BY '.$columns[$_POST['order']['0']['column']].' '.$_POST['order']['0']['dir'].' 
+        ';
+    } else {
+        $query .= ' ORDER BY Status ASC ';
+    }
+
+    $query1 = '';
+
+    if ($_POST["length"] != -1) {
+        $query1 .= 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+    }
+
+    $number_filter_row = mysqli_num_rows(mysqli_query($db, $query));
+
+    $result = mysqli_query($db, $query . $query1);
+
+    $data = array();
+
+    while ($row = mysqli_fetch_array($result)) {
+        $sub_array = array();
+        $status = '';
+        if ($row['Status'] == 0) {
+            $status = "Pending";
+        } else if ($row['Status'] == 1) {
+            $status = "Approved";
+        } else {
+            $status = "Denied";
+        }
+
+        $sub_array[] = '<div class="update" data-id="'.$row["BusinessID"].'" data-column="'.$row['BusinessName'].'"><a href="#" id="profile-name">'.$row['BusinessName'].'</a></div>';
+        $sub_array[] = '<div class="update" data-id="'.$row["BusinessID"].'" data-column="'.$row['Status'].'">'.$status.'</div>';
+        $sub_array[] = '<div class="btn-group" role="group" data-id="'.$row["BusinessID"].'">
+                        <button type="button" name="approve" id="approve" data-toggle="tooltip" title="Approve User" class="btn btn-success">Approve</button>
+                        <button type="button" name="pending" id="pending" data-toggle="tooltip" title="User Pending" class="btn btn-warning">Pending</button>
+                        <button type="button" name="deny" id="deny" data-toggle="tooltip" title="Deny User" class="btn btn-danger">Deny</button></div></td>';
+
+        $data[] = $sub_array;   
+    }
+
+    function get_all_data($db) {
+        $query = "SELECT * FROM Business";
+        $result = mysqli_query($db, $query);
+        return mysqli_num_rows($result);
+    }
+
+    $output = array(
+        "draw"              =>  intval($_POST["draw"]),
+        "recordsTotal"      =>  get_all_data($db),
+        "recordsFiltered"   =>  $number_filter_row,
+        "data"              =>  $data
+    );
+
+    echo json_encode($output);
+   
+    // venue table
+} else if ($_POST['table'] == 'Venue') {
+    $columns = array('VenueName', 'Status');
+
+    $query = "SELECT * FROM Venue ";
+
+
+    if(isset($_POST["search"]["value"])) {
+        $query .= ' 
+            WHERE VenueName LIKE "%'.$_POST["search"]["value"].'%"
+            OR Status LIKE "%'.$_POST["search"]["value"].'%" 
+            ';
+    }
+
+    if (isset($_POST["order"])) {
+        $query .= ' ORDER BY '.$columns[$_POST['order']['0']['column']].' '.$_POST['order']['0']['dir'].' 
+        ';
+    } else {
+        $query .= ' ORDER BY Status ASC ';
+    }
+
+    $query1 = '';
+
+    if ($_POST["length"] != -1) {
+        $query1 .= 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+    }
+
+    $number_filter_row = mysqli_num_rows(mysqli_query($db, $query));
+
+    $result = mysqli_query($db, $query . $query1);
+
+    $data = array();
+
+    while ($row = mysqli_fetch_array($result)) {
+        $sub_array = array();
+        $status = '';
+        if ($row['Status'] == 0) {
+            $status = "Pending";
+        } else if ($row['Status'] == 1) {
+            $status = "Approved";
+        } else {
+            $status = "Denied";
+        }
+
+        $sub_array[] = '<div class="update" data-id="'.$row["VenueID"].'" data-column="'.$row['VenueName'].'"><a href="#" id="profile-name">'.$row['VenueName'].'</a></div>';
+        $sub_array[] = '<div class="update" data-id="'.$row["VenueID"].'" data-column="'.$row['Status'].'">'.$status.'</div>';
+        $sub_array[] = '<div class="btn-group" role="group" data-id="'.$row["VenueID"].'">
+                        <button type="button" name="approve" id="approve" data-toggle="tooltip" title="Approve User" class="btn btn-success">Approve</button>
+                        <button type="button" name="pending" id="pending" data-toggle="tooltip" title="User Pending" class="btn btn-warning">Pending</button>
+                        <button type="button" name="deny" id="deny" data-toggle="tooltip" title="Deny User" class="btn btn-danger">Deny</button></div></td>';
+
+        $data[] = $sub_array;   
+    }
+
+    function get_all_data($db) {
+        $query = "SELECT * FROM Venue";
+        $result = mysqli_query($db, $query);
+        return mysqli_num_rows($result);
+    }
+
+    $output = array(
+        "draw"              =>  intval($_POST["draw"]),
+        "recordsTotal"      =>  get_all_data($db),
+        "recordsFiltered"   =>  $number_filter_row,
+        "data"              =>  $data
+    );
+
+    echo json_encode($output);
 }
-
-if (isset($_POST["order"])) {
-    $query .= ' ORDER BY '.$columns[$_POST['order']['0']['column']].' '.$_POST['order']['0']['dir'].' 
-    ';
-} else {
-    $query .= ' ORDER BY VenueName ASC ';
-}
-
-$query1 = '';
-
-if ($_POST["length"] != -1) {
-    $query1 .= 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
-}
-
-$number_filter_row = mysqli_num_rows(mysqli_query($db, $query));
-
-$result = mysqli_query($db, $query . $query1);
-
-$data = array();
-
-while ($row = mysqli_fetch_array($result)) {
-    $sub_array = array();
-    $sub_array[] = '<div class="update" data-id="'.$row["VenueID"].'" data-column="'.$row['VenueName'].'"><a href="#" id="profile-name">'.$row['VenueName'].'</a></div>';
-    $sub_array[] = '<div class="update" data-id="'.$row["VenueID"].'" data-column="'.$row['Capacity'].'">'.$row['Capacity'].'</div>';
-    $sub_array[] = '<div class="update" data-id="'.$row["VenueID"].'" data-column="'.$row['PhoneNumber'].'">'.$row['PhoneNumber'].'</div>';
-    $sub_array[] = '<div class="update" data-id="'.$row["VenueID"].'" data-column="'.$row['Email'].'">'.$row['Email'].'</div>';
-    $sub_array[] = '<div class="update" data-id="'.$row["VenueID"].'" data-column="'.$row['City'].'">'.$row['City'].'</div>';        
-    $sub_array[] = '<div class="update" data-id="'.$row["VenueID"].'" data-column="'.$row['State'].'">'.$row['State'].'</div>';
-    $sub_array[] = '<div class="update" data-id='
-
-    $data[] = $sub_array;   
-}
-
-function get_all_data($db) {
-    $query = "SELECT * FROM Venue WHERE Status = 1";
-    $result = mysqli_query($db, $query);
-    return mysqli_num_rows($result);
-}
-
-$output = array(
-    "draw"              =>  intval($_POST["draw"]),
-    "recordsTotal"      =>  get_all_data($db),
-    "recordsFiltered"   =>  $number_filter_row,
-    "data"              =>  $data
-);
-
-echo json_encode($output);
-
 ?>
-
